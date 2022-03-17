d7d72c4220170cd836675bf785070ce5aa9dafe2
ludwig <murimiludwig@gmail.com>
2022-03-17 07:29:51 +0300

    this will modify the lay out

--- script.js
+++ script.js
@@ -24,72 +24,96 @@ $(document).ready(function(){
         $(".product-details").hide();
     })
 });
-// 
-// $(document).ready(function(){
-    // $("#hover1").hover(function(){
-        // $("#hover-it").show();
-    // },function(){
-        // $("hover-it").hide();
-    // })
-    // $("#hover2").hover(function(){
-        // $("#hover-it").show();
-    // },function(){
-        // $("hover-it").hide();
-    // })
-    // $("#hover3").hover(function(){
-        // $("#hover-it3").show();
-    // },function(){
-        // $("hover-it3").hide();
-    // })
-    // $("#hover4").hover(function(){
-        // $("#hover-it4").show();
-    // },function(){
-        // $("hover-it4").hide();
-    // })
-    // $("#hover5").hover(function(){
-        // $("#hover-it5").show();
-    // },function(){
-        // $("hover-it5").hide();
-    // })
-    // $("#hover6").hover(function(){
-        // $("#hover-it6").show();
-    // },function(){
-        // $("hover-it6").hide();
-    // })
-    // $("#hover7").hover(function(){
-        // $("#hover-it7").show();
-    // },function(){
-        // $("hover-it7").hide();
-    // })
-    // $("#hover8").hover(function(){
-        // $("#hover-it8").show();
-    // },function(){
-        // $("hover-it8").hide();
-    // })
-// 
+ 
 
-// });
 
-// $(".hover").hover(function(){
-//     $(this).animate({opacity:'1'});
-//   },
-//   function(){
-//     $(this).animate({opacity:'0'});
-//   })
 
-$('#subscribe').click(function () {
+
+
+
+
+
+
+ $(document).ready(function(){
+   $("#hover1").hover(function(){
+    $("#hover-it1").show();
+     },function(){
+      $("#hover-it").hide();
+     })
+     $("#hover2").hover(function(){
+      $("#hover-it").show();
+     },function(){
+      $("hover-it").hide();
+     })
+     $("#hover3").hover(function(){
+      $("#hover-it3").show();
+     },function(){
+      $("hover-it3").hide();
+     })
+     $("#hover4").hover(function(){
+       $("#hover-it4").show();
+     },function(){
+       $("hover-it4").hide();
+     })
+     $("#hover5").hover(function(){
+       $("#hover-it5").show();
+     },function(){
+       $("hover-it5").hide();
+     })
+     $("#hover6").hover(function(){
+       $("#hover-it6").show();
+     },function(){
+       $("hover-it6").hide();
+     })
+     $("#hover7").hover(function(){
+       $("#hover-it").show();
+     },function(){
+       $("hover-it").hide();
+     })
+     $("#hover8").hover(function(){
+       $("#hover-it").show();
+     },function(){
+       $("hover-it8").hide();
+     })
+ }); 
+
+
+
+ /* $(".hover").hover(function(){
+     $(this).animate({opacity:'1'});
+   },
+   function(){
+     $(this).animate({opacity:'0'});
+   }) */
+
+   
+   $(".work-image").mousemove(function (e) {
+    var hovertext = $(this).attr("hovertext");
+    $(this).css("border", "2px solid grey", "border", "11px solid white");
+    $(".display").text(hovertext).show().offset($(this).offset());
+  
+  }).mouseout(function () {
+    $(this).css("border", "none");
+    $('.display').hide()
+  });
+
+
+
+
+
+
+
+
+
+
+$(".button").click(function () {
       alert()
-  var Name = $('#mce-NAME').val();
-  var Email = $('#mce-EMAIL').val();
-  var Message = $('#mce-MESSAGE');
+  var Name = $("#me-name").val();
+  var Email = $("#me-email").val();
+  var Message = $("#the-comment");
     if (Name == '' || Email == '' || Message == '') {
       alert('Please make sure you have filled in the form correctly!');
   } else {
       alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
   }
 });
-// let button = document.getElementById("subscribe")
-// button.addEventListener("click", (e)=>{
-    // e.preventDefault();
-    // alert("")
-// })
